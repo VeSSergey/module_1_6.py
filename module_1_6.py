c3c7c7aef66a45096f0cb262f94cd84837345821
VeSSergey <vesrzo@gmail.com>
2024-08-09 17:42:12 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,21 @@
+#2HW
+my_dict = {'Sergey': 1990, 'Denis': 1994, 'Olga': 1991}
+print(my_dict)
+print(my_dict['Sergey'])
+my_dict ['Ivan'] = 1992
+print(my_dict)
+my_dict.update({'Rafic': 1875,
+                'Zayr': 1775})
+print(my_dict)
+my_dict.pop('Olga')
+print(my_dict)
+print(my_dict)
+#3HW
+my_set = {8, 8, 2, 4, 9, 1, 4, False, 'Blueberry' }
+print(my_set)
+my_set = {8, 8, 2, 4, 9, 1, 4, False, 'Blueberry', (1, 2)}
+print(my_set)
+my_set = set(my_set)
+print(my_set)
+print(my_set.remove(0))
+print(my_set)
